4e3e099e2c36bd8796ddc9342f49efa213b35131
Troy <troyd9216@gmail.com>
2021-10-17 17:51:53 -0500

    fixed import error

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const specs = swaggerJsDoc(options)
 // middlewares
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use(cors())
-app.use(morgan())
+app.use(morgan('combined'))
 app.use(express.json())
 app.use(routes) // localhost:3000
 
--- models/Repair.js
+++ models/Repair.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-
 const RepairsSchema = Schema({
     customerEmail: {
         type: String,
