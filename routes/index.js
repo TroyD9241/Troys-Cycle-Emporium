88a8a98bd8f62637c0f03f0fea0cfb53d5ca3388
Troy <troyd9216@gmail.com>
2021-10-18 00:37:41 -0500

    tried fixing bug, did some code cleanup, added connection events, and added some comments

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
-const apiRouter = require('./api');
-
 //localhost:3000
+const router = require('express').Router();
+// importing api index
+const apiRouter = require('./api/index');
 
 router.use('/api', apiRouter);
 //localhost:3000/api
